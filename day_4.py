5f5148f19e8c0513d5a2b7784736f61bacf9916d
Dileep Sathyan <dileepsath@gmail.com>
2022-05-05 20:10:37 +0530

    Pandas Basics

---
+++ day_4.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Import a csv file using Pandas
+df1 = pd.read_csv('/Users/dileepsathyan/Downloads/Churn_Modelling.xls')
+print(df1)
+
+# Check the size of the DataFrame using 'shape' function
+#print(df1.shape)
+
+#

803901705af05a797386fd9d201205639a5a7e72
Dileep Sathyan <dileepsath@gmail.com>
2022-05-10 21:19:54 +0530

    Pandas Visualisations 2

--- day_9.py
+++ day_9.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 x = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 y = [1, 4, 3, 2, 7, 6, 9, 8, 10, 5]
 
-# plt.plot(x, y)
-# plt.title('Line Graph')
-# print(plt.show())
+plt.plot(x, y)
+plt.title('Line Graph')
+print(plt.show())
 
 
 # Create the values and a figure
@@ -18,24 +18,24 @@ x1 = np.linspace(0, 10, 100)
 fig = plt.figure()
 
 # Plot a sine and cosine wave
-# plt.plot(x1, np.sin(x1), '-')
-# plt.plot(x1, np.cos(x1), '--');
-# print(plt.show())
+plt.plot(x1, np.sin(x1), '-')
+plt.plot(x1, np.cos(x1), '--');
+print(plt.show())
 
 
 # Create Subplots and configure the sine and cosine waves individually.
-# plt.subplot(2,1,1) #(row_count, col_count, panel_count)
-# plt.plot(x1, np.sin(x1))
+plt.subplot(2,1,1) #(row_count, col_count, panel_count)
+plt.plot(x1, np.sin(x1))
 
-# plt.subplot(2,1,2) #(row_count, col_count, panel_count)
-# plt.plot(x1, np.cos(x1))
+plt.subplot(2,1,2) #(row_count, col_count, panel_count)
+plt.plot(x1, np.cos(x1))
 
-# plt.show()
+plt.show()
 
 
 # Plot is a very versatile command 
-# plt.plot([1, 2, 3, 4], [1, 4, 8, 16])
-# plt.show()
+plt.plot([1, 2, 3, 4], [1, 4, 8, 16])
+plt.show()
 
 
 # Pyplot provides the state-machine interface to the underlying object-oriented plotting library. 

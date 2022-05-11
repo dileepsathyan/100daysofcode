8e03e4e5eccbeac6a8ddf534428dba50466ded12
Dileep Sathyan <dileepsath@gmail.com>
2022-05-11 20:08:21 +0530

    Update day_10.py

--- day_10.py
+++ day_10.py
@@ -27,8 +27,24 @@ data2 = [5. , 25. , 50. , 20.]
 a = [15., 30., 45., 22.]
 b = [15., 25., 50., 20.]
 z1 = range(4)
+# plt.bar(z1, a, color = 'b')
+# plt.bar(z1, b, color = 'r', bottom = a)
+# plt.title('Stacked Bar Chart')
+# plt.show()
 
-plt.bar(z1, a, color = 'b')
-plt.bar(z1, b, color = 'r', bottom = a)
-plt.title('Stacked Bar Chart')
-plt.show()
+
+# Pie Chart
+x1 = [35, 25, 20, 20]
+labels = ['Computer', 'Electronics', 'Mechanical', 'Chemical']
+# plt.figure(figsize=(5,5))
+# plt.pie(x1, labels=labels)
+# plt.title('Pie Chart')
+# plt.show()
+
+
+# Box Plot
+data3 = np.random.randn(100)
+# plt.boxplot(data3)
+# plt.title('Box Chart')
+# plt.show()
+

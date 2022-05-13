5e5eb23fc260afa401fa95dd2f2edee942765c85
Dileep Sathyan <dileepsath@gmail.com>
2022-05-13 18:24:38 +0530

    Update day_12.py

--- day_12.py
+++ day_12.py
@@ -16,8 +16,14 @@ func1 = lambda a : a+10
 # print(func1(5))
 
 
-# Create a lambda function to add 10 to 2 arguments
+# Create a lambda function with 2 arguments
 
 func2 = lambda a, b : a*b
 # print(func2(2, 4))
+
+
+# Create a lambda function with 3 arguments
+func3 = lambda a, b, c: (a + b)**c
+print(func3(3, 4, 2))
+
 

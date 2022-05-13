2bbf0c782dba61d35e00cfee8a52baa3939ed939
Dileep Sathyan <dileepsath@gmail.com>
2022-05-13 18:17:42 +0530

    Create day_12.py

---
+++ day_12.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# LAMBDA FUNCTIONS
+
+# Lambda functions are usually small anonymous functions which takes any number of arguments but only 1 expression.
+# These functions are powerful when combined with other functions.
+
+# Syntax of Lambda functions
+
+lambda argument : expression 
+
+

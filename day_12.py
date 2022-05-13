1ab97a8e65616322fc71116837e34744524e82a5
Dileep Sathyan <dileepsath@gmail.com>
2022-05-13 18:28:41 +0530

    Update day_12.py

--- day_12.py
+++ day_12.py
@@ -22,6 +22,13 @@ func2 = lambda a, b : a*b
 
 # Create a lambda function with 3 arguments
 func3 = lambda a, b, c: (a + b)**c
-print(func3(3, 4, 2))
+# print(func3(3, 4, 2))
 
+# Create a default lambda function and reuse it to create more functions.
+def func4(n):
+    return lambda a: a * n
 
+func5_doubler = func4(2)
+func6_tripler = func4(3)
+print(func5_doubler(4))
+print(func6_tripler(7))

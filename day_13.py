b8c8128123342e08827051899d939cfc0504b077
Dileep Sathyan <dileepsath@gmail.com>
2022-05-14 16:49:05 +0530

    Update day_13.py

--- day_13.py
+++ day_13.py
@@ -7,3 +7,15 @@ import pandas as pd
 # Parent class is the class being inherited from, also called base class.
 # Child class is the class that inherits from another class, also called derived class.
 
+# Create a Parent Class
+
+class Parent:
+    def __init__(self, first_name, last_name):
+        self.fname = first_name
+        self.lname = last_name
+    
+    def print_name(self):
+        print('Hi, '+ self.fname, self.lname + '!..')
+
+p1 = Parent('Dileep', 'Sathyan')
+p1.print_name()    

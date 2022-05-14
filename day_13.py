09cbf0f0d11e3e0965a2a0dd12e6bd8131a4d765
Dileep Sathyan <dileepsath@gmail.com>
2022-05-14 17:13:07 +0530

    Update day_13.py

--- day_13.py
+++ day_13.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from day_12 import Person
+
 
 # INHERITANCE
 
@@ -62,5 +64,18 @@ class Student3(Parent):
         super().__init__(first_name, last_name)
         self.graduation_year = 2008
 
-p4 = Student3('Dileep', 'Trinay')
-print(p4.graduation_year)
+# p4 = Student3('Dileep', 'Trinay')
+# print(p4.graduation_year)
+
+
+
+# In the example below, the year 2019 should be a variable, and passed into the Student class when creating student objects. 
+# To do so, add another parameter in the __init__() function.
+
+class Student4(Person):
+    def __init__(self, first_name, last_name, year):
+        super().__init__(first_name, last_name, year)
+        self.graduation_year = year
+
+p5 = Student4('Dileep', 'Trinay', 2008)
+print(p5.graduation_year)

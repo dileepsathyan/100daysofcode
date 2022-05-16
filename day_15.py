001d178fd177a2b69b9c63c0b6466efb55f347f9
Dileep Sathyan <dileepsath@gmail.com>
2022-05-16 08:35:12 +0530

    Create day_15.py

---
+++ day_15.py
@@ -0,0 +1,16 @@
+import math
+
+# Built-in math functions in Python
+
+# Find the MIN and MAX
+x = min(2, 6, 4)
+y = max(3, 10, 8)
+
+# print(x)
+# print(y)
+
+
+
+# Find the SUM of values
+z = x + y
+print(z)

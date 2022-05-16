abc962095ff7bb0efecb6016983eef00c5f35437
Dileep Sathyan <dileepsath@gmail.com>
2022-05-16 08:38:13 +0530

    Update day_15.py

--- day_15.py
+++ day_15.py
@@ -1,16 +1,29 @@
-import math
-
 # Built-in math functions in Python
 
 # Find the MIN and MAX
 x = min(2, 6, 4)
 y = max(3, 10, 8)
-
 # print(x)
 # print(y)
 
 
-
 # Find the SUM of values
 z = x + y
-print(z)
+# print(z)
+
+
+# Find the ABSOLUTE value
+a = -3.142
+# print(abs(a))
+
+
+# Calculate the Power of a number
+sq = pow(5,2)
+# print(sq)
+
+
+import math
+
+
+# MATH MODULE
+

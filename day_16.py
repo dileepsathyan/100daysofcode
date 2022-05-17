433f2aa7e0db16a7d007dddaea58682acf7da923
Dileep Sathyan <dileepsath@gmail.com>
2022-05-17 17:33:07 +0530

    Create day_16.py

---
+++ day_16.py
@@ -0,0 +1,6 @@
+import json
+
+# JSON is a syntax for storing and exchanging data. JSON is text, written with JavaScript object notation.
+# Python has a built-in package called json, which can be used to work with JSON data.
+
+# Convert JSON string in Python using the json.loads() method.

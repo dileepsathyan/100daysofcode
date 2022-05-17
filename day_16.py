ad8c65963e75112b4d86f71861e5e0259f103ff6
Dileep Sathyan <dileepsath@gmail.com>
2022-05-17 17:40:30 +0530

    Update day_16.py

--- day_16.py
+++ day_16.py
@@ -3,6 +3,8 @@ import json
 # JSON is a syntax for storing and exchanging data. JSON is text, written with JavaScript object notation.
 # Python has a built-in package called json, which can be used to work with JSON data.
 
+
+
 # Convert JSON string in Python using the json.loads() method.
 
 x = '{"name": "Dileep", "age": 30, "department": "Science"}'
@@ -13,9 +15,12 @@ print(x_file["age"])
 print(x_file["department"])
 
 
+
 # Convert a Python Object to a JSON string by using the json.dumps() method.
 
 y = json.dumps(x_file)
 print(y)
 print(x)
+
+
 

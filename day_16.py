f1e0614519e7d91d6033c43a4ba5b7a972ef6b94
Dileep Sathyan <dileepsath@gmail.com>
2022-05-17 17:44:25 +0530

    Update day_16.py

--- day_16.py
+++ day_16.py
@@ -24,4 +24,17 @@ y = json.dumps(x_file)
 
 
 
-print(json.dumps(["apple", "bananas"]))
+j = {
+    "name": "John",
+    "age": 30,
+    "married": True,
+    "divorced": False,
+    "children": ("Ann","Billy"),
+    "pets": None,
+    "cars": [
+            {"model": "BMW 230", "mpg": 27.5},
+            {"model": "Ford Edge", "mpg": 24.1}
+            ]
+    }
+
+print(json.dumps(j))

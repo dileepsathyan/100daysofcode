8347f3b10a9e02600b916bf9b0532d8b3941c04e
Dileep Sathyan <dileepsath@gmail.com>
2022-05-18 19:35:57 +0530

    Update day_17.py

--- day_17.py
+++ day_17.py
@@ -4,3 +4,25 @@ import re
 # RegEx can be used to check if a string contains the specified search pattern.
 # Python has a built-in package called re, which can be used to work with Regular Expressions.
 
+
+txt = 'The rain in Spain'
+
+x = re.search('^The.*Spain$', txt)
+if x:
+    print('We have a match!')
+else:
+    print('No match...')
+
+
+
+# Character	Description	                    Example	
+# []	    A set of characters	            "[a-m]"
+# \	        Signals a special sequence (can also be used to escape special characters)	"\d"
+# .	        Any character (except newline character)	"he..o"
+# ^	        Starts with	                    "^hello"
+# $	        Ends with	                    "planet$"
+# *	        Zero or more occurrences	    "he.*o"
+# +	        One or more occurrences	        "he.+o"
+# ?	        Zero or one occurrences	        "he.?o"
+# {}	    Exactly the specified number of occurrences	"he.{2}o"
+# |	        Either or	                    "falls|stays"

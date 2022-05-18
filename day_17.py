82519eced2d36beb3a5f989fe80236b99d921993
Dileep Sathyan <dileepsath@gmail.com>
2022-05-18 19:40:31 +0530

    Update day_17.py

--- day_17.py
+++ day_17.py
@@ -5,6 +5,17 @@ import re
 # Python has a built-in package called re, which can be used to work with Regular Expressions.
 
 
+# The re module offers a set of functions that allows us to search a string for a match:
+
+# Function	    Description
+# findall	    Returns a list containing all matches
+# search	    Returns a Match object if there is a match anywhere in the string
+# split	        Returns a list where the string has been split at each match
+# sub	        Replaces one or many matches with a string
+
+
+
+
 txt = 'He passed in IELTS Exam'
 
 x = re.search('^He.*Exam$', txt)
@@ -26,3 +37,6 @@ else:
 # ?	        Zero or one occurrences	        "he.?o"
 # {}	    Exactly the specified number of occurrences	"he.{2}o"
 # |	        Either or	                    "falls|stays"
+
+
+

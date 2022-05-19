f6b944645989d1df8a8250214d4a711e34a7750a
Dileep Sathyan <dileepsath@gmail.com>
2022-05-19 19:26:22 +0530

    Exception Handling

--- day_18.py
+++ day_18.py
@@ -9,42 +9,42 @@ from numpy import dtype
 
 # The try block will generate an exception, because x is not defined
 
-# try:
-#     print(x)
-# except:
-#     print('Variable x was not defined but the exception has been handled...')
+try:
+    print(x)
+except:
+    print('Variable x was not defined but the exception has been handled...')
 
 
 # Many Exceptions: You can define as many exception blocks as you want.
 # e.g. if you want to execute a special block of code for a special kind of error:
 
-# try:
-#     print(y)
-# except NameError:
-#     print('y is not defined..')
-# except:
-#     print('some other error occured..')
+try:
+    print(y)
+except NameError:
+    print('y is not defined..')
+except:
+    print('some other error occured..')
 
 
 # Else: You can use the else keyword to define a block of code to be executed if no errors were raised.
 
-# try:
-#     print('Hi')
-# except:
-#     print('This is the except block..')
-# else:
-#     print('This is else block which means there are no errors found...')
+try:
+    print('Hi')
+except:
+    print('This is the except block..')
+else:
+    print('This is else block which means there are no errors found...')
 
 
 
 # Finally: The finally block, if specified, will be executed regardless if the try block raises an error or not
 
-# try:
-#     print(x)
-# except:
-#     print('This is the except block because x was not defined..')
-# finally:
-#     print('This is finally block which will be exceuted no matter what')
+try:
+    print(x)
+except:
+    print('This is the except block because x was not defined..')
+finally:
+    print('This is finally block which will be exceuted no matter what')
 
 
 

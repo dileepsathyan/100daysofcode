940d2e085bf53accc500b87735e9148948f0b2e4
Dileep Sathyan <dileepsath@gmail.com>
2022-05-20 14:45:53 +0530

    Update day_19.py

--- day_19.py
+++ day_19.py
@@ -32,6 +32,11 @@ txt4 = 'I bought {2} cars at {0} rupees each but I had to sell them for {1} rupe
 
 
 
+# Explicit names in the variables to assign values
+
+
+
+
 # Format the numbers in a string
 
 txt5 = 'It costs {:.2f} dollars'

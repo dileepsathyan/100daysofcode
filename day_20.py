1ecb01af5a8bf91864a008cd28781057ae26acfa
Dileep Sathyan <dileepsath@gmail.com>
2022-05-21 18:14:16 +0400

    Update day_20.py

--- day_20.py
+++ day_20.py
@@ -1,8 +1,18 @@
 # While Loops
 
 # A while loop executes a set of statements as long as a condition is true.
-
+# The while loop requires relevant variables to be ready, in the below example we need to define an indexing variable, i, which we set to 1 intially.
 i = 1
 while i <= 5:
     print(i)
     i += 1
+
+
+# Break statement helps stop the loop even if the while condition is true:
+
+j = 10
+while j < 15:
+    print(j)
+    if j == 13:
+        break
+    j +=1

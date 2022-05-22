5e14f99850b20fee00cf8b6a663c7ddd9fa5ce0f
Dileep Sathyan <dileepsath@gmail.com>
2022-05-22 19:14:11 +0400

    Create day_21.py

---
+++ day_21.py
@@ -0,0 +1,15 @@
+# Handling Strings in Python
+
+# Charactors in Strings can be called using their indexes.
+str1 = 'I am from India'
+# print(str1[2])
+
+
+# Looping through strings
+# for i in str1:
+#     print(i)
+
+
+# Length of a string
+# print(len(str1))
+

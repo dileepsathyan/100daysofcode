a2cb32ece2c69f8eac48cb02924f5c5cec6e0d1d
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-23 18:10:25 +0400

    MySQL Database Conn 1

--- day_22.py
+++ day_22.py
@@ -17,62 +17,62 @@ mycursor.execute("CREATE DATABASE sample_db")
 # View Available Databases
 mycursor.execute("SHOW DATABASES")
 
-# for x in mycursor:
-#     print(x)
+for x in mycursor:
+    print(x)
 
 
 # Connecting to the database while making the MySQL connection
-# mydb = mysql.connector.connect(
-#                                 host="localhost",
-#                                 user="root",
-#                                 password="password",
-#                                 database='sample_db'
-#                               )
+mydb = mysql.connector.connect(
+                                host="localhost",
+                                user="root",
+                                password="password",
+                                database='sample_db'
+                              )
 
 
 
 # Creating a new table
-# mydb = mysql.connector.connect(
-#                                 host="localhost",
-#                                 user="root",
-#                                 password="password",
-#                                 database='sample_db'
-#                               )
-# mycursor = mydb.cursor()
+mydb = mysql.connector.connect(
+                                host="localhost",
+                                user="root",
+                                password="password",
+                                database='sample_db'
+                              )
+mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), age TINYINT(255))")
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), age TINYINT(255))")
 
 
 
 # # View tables in a database
-# mycursor.execute("SHOW TABLES")
+mycursor.execute("SHOW TABLES")
 
-# for x in mycursor:
-#     print(x)
+for x in mycursor:
+    print(x)
 
 
 
 # Inserting a record to the table
 
-# sql = "INSERT INTO customers (name, age) VALUES (%s, %s)"
-# val = ("Dileep", 31)
+sql = "INSERT INTO customers (name, age) VALUES (%s, %s)"
+val = ("Dileep", 31)
 
-# mycursor.execute(sql, val)
+mycursor.execute(sql, val)
 
-# mydb.commit()
+mydb.commit()
 
-# print(mycursor.rowcount, "record inserted.")
+print(mycursor.rowcount, "record inserted.")
 
 
 
 
 # Inserting multiple records at once.
 
-# sql = "INSERT INTO customers (name, age) VALUES (%s, %s)"
-# val = [("Dileep", 31), ("Sajana", 29), ("Trinay", 3)]
+sql = "INSERT INTO customers (name, age) VALUES (%s, %s)"
+val = [("Dileep", 31), ("Sajana", 29), ("Trinay", 3)]
 
-# mycursor.executemany(sql, val)
+mycursor.executemany(sql, val)
 
-# mydb.commit()
+mydb.commit()
 
-# print(mycursor.rowcount, "records were inserted.")
+print(mycursor.rowcount, "records were inserted.")

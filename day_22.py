49bbdad93ac2ade77863a1afee87bf92b3807295
Dileep Sathyan <dileep@opendatafabric.com>
2022-05-23 16:43:12 +0400

    Update day_22.py

--- day_22.py
+++ day_22.py
@@ -1,2 +1,22 @@
 # MySQL using Python
 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+                                host="localhost",
+                                user="root",
+                                password="password"
+                              )
+                              
+
+# Creating a new database
+mycursor = mydb.cursor()
+# mycursor.execute("CREATE DATABASE sample_db")
+
+
+# View Available Databases
+mycursor.execute("SHOW DATABASES")
+for x in mycursor:
+    print(x)
+
+

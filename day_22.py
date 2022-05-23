8330278cc0de328ad421b2ee7e394b63ddcac259
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-23 17:01:44 +0400

    Update day_22.py

--- day_22.py
+++ day_22.py
@@ -2,20 +2,20 @@
 
 import mysql.connector
 
-# mydb = mysql.connector.connect(
-#                                 host="localhost",
-#                                 user="root",
-#                                 password="password"
-#                               )
+mydb = mysql.connector.connect(
+                                host="localhost",
+                                user="root",
+                                password="password"
+                              )
                               
 
 # Creating a new database
-# mycursor = mydb.cursor()
-# mycursor.execute("CREATE DATABASE sample_db")
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE sample_db")
 
 
 # View Available Databases
-# mycursor.execute("SHOW DATABASES")
+mycursor.execute("SHOW DATABASES")
 
 # for x in mycursor:
 #     print(x)
@@ -32,13 +32,13 @@ import mysql.connector
 
 
 # Creating a new table
-mydb = mysql.connector.connect(
-                                host="localhost",
-                                user="root",
-                                password="password",
-                                database='sample_db'
-                              )
-mycursor = mydb.cursor()
+# mydb = mysql.connector.connect(
+#                                 host="localhost",
+#                                 user="root",
+#                                 password="password",
+#                                 database='sample_db'
+#                               )
+# mycursor = mydb.cursor()
 
 # mycursor.execute("CREATE TABLE customers (name VARCHAR(255), age TINYINT(255))")
 
@@ -51,12 +51,28 @@ mycursor = mydb.cursor()
 #     print(x)
 
 
+
 # Inserting a record to the table
 
-sql = "INSERT INTO customers (name, age) VALUES (%s, %s)"
-val = ("Dileep", 31)
-mycursor.execute(sql, val)
+# sql = "INSERT INTO customers (name, age) VALUES (%s, %s)"
+# val = ("Dileep", 31)
 
-mydb.commit()
+# mycursor.execute(sql, val)
 
-print(mycursor.rowcount, "record inserted.")
+# mydb.commit()
+
+# print(mycursor.rowcount, "record inserted.")
+
+
+
+
+# Inserting multiple records at once.
+
+# sql = "INSERT INTO customers (name, age) VALUES (%s, %s)"
+# val = [("Dileep", 31), ("Sajana", 29), ("Trinay", 3)]
+
+# mycursor.executemany(sql, val)
+
+# mydb.commit()
+
+# print(mycursor.rowcount, "records were inserted.")

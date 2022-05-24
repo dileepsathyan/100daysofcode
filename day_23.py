298dccd0c986708ff6944da52103e84f28365025
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-24 12:11:47 +0400

    Update day_23.py

--- day_23.py
+++ day_23.py
@@ -3,6 +3,12 @@ import mysql.connector
 mydb = mysql.connector.connect(
                             host='localhost',
                             user= 'root',
-                            password='password'
+                            password='password',
+                            database='sample_db'
                             )
 
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM CUSTOMERS")
+
+for x in mycursor:
+    print(x)

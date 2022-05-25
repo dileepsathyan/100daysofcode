d5e7497278791bad4f7bab5194c30cf685da4d28
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-25 19:10:15 +0400

    Update day_24.py

--- day_24.py
+++ day_24.py
@@ -1,13 +1,39 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
+mydb1 = mysql.connector.connect(
                             host='localhost',
                             username='root',
                             password='password'
                             )
 
-qry1 = "SHOW DATABASES"
-with mydb.cursor() as cursor:
-    cursor.execute(qry1)
-    for db in cursor:
-        print(db)
+# qry1 = "SHOW DATABASES"
+
+# with mydb1.cursor() as cursor:
+#     cursor.execute(qry1)
+#     for db in cursor:
+#         print(db)
+
+
+# Connecting to an existing database
+mydb2 = mysql.connector.connect(
+                            host='localhost',
+                            username='root',
+                            password='password',
+                            database='sample_db'
+                        )
+qry2 = "SHOW TABLES"
+
+# with mydb2.cursor() as cursor:
+#     cursor.execute(qry2)
+#     for tbl_name in cursor:
+#         print(tbl_name)
+
+
+# Describing a table
+qry3 = "DESCRIBE relation"
+
+with mydb2.cursor() as cursor:
+    cursor.execute(qry3)
+    result2 = cursor.fetchall()
+    for field in result2:
+        print(field)

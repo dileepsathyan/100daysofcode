6aa4ee1e76c01388f74d4d57edb22c51ee9cce69
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-25 18:36:50 +0400

    Update day_24.py

--- day_24.py
+++ day_24.py
@@ -3,7 +3,9 @@ import mysql.connector
 mydb = mysql.connector.connect(
                             host='localhost',
                             username='root',
-                            password='password'
+                            password='password',
+                            database='sample_db'
                             )
 
-                            
+mycursor = mydb.cursor()
+

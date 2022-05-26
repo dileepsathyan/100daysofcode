6f9bf17d828b2477fc76427b8c62d7a35d74e742
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-26 17:48:08 +0400

    Update day_25.py

--- day_25.py
+++ day_25.py
@@ -3,3 +3,12 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+
+# Lets define the input x and response y.
+# .reshape() makes the input variables 2-D (1 column vs many rows)
+
+x = np.array([21, 24, 27, 30, 33, 36, 39]).reshape((-1, 1))
+y = np.array([4, 6, 8, 10, 12, 14, 16])
+
+# print(x)
+# print(y)

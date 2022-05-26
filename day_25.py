c4fa1b6bcbf94a1dc6b14f66a8ca2fba161fe4a0
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-26 18:08:00 +0400

    Update day_25.py

--- day_25.py
+++ day_25.py
@@ -40,3 +40,11 @@ print(f"Slope or Co-eff Value : {slope}")
 # Intercept of -3.928 is the y value when the x is zero.
 # Slope / co-eff value represents the y value increases by 0.44 units for 1 unit increase in x value.
 
+# Since the response variable y is 1-D in the above example, the intercept is a scalar value & the slope is an array with just 1 variable.
+
+# In case the response variable is 2-D, we will get the Intercept also as an array. Lets verify that in the below example by making both input and response as 2-D.
+p = np.array([21, 24, 27, 30, 33, 36, 39]).reshape((-1, 1))
+q = np.array([4, 7, 10, 8, 12, 11, 13])
+print(p)
+print(q)
+

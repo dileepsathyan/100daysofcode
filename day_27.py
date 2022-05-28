4ed106f8f5f10f810407ab0e247084221b769c90
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-28 16:06:33 +0400

    Update day_27.py

--- day_27.py
+++ day_27.py
@@ -5,5 +5,14 @@ import statsmodels.api as sm
 
 # LINEAR REGRESSION USING STATSMODELS.
 
+# Statsmodel is preferred to analyse Linear Regression when more detailed results are required. The process is similar to that of SKLEARN.
+
+# First import the datasets & define the variables.
+file = pd.read_csv("/Users/dileepsathyan/Documents/GitHub/datasets/Fish.csv")
+file = file[file['Species']=='Bream']
+
+x = file[['Length', 'Height', 'Width']]
+y = file['Weight']
+
 
 

908d225fd133c6af7130c68a98895bb16a9e8cf5
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-29 10:53:06 +0400

    Update day_28.py

--- day_28.py
+++ day_28.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
+
 # LOGISTIC REGRESSION
 
 # Classification is an area of supervised machine learning that tries to predict which class or category some entity belongs to, based on its features
@@ -10,4 +11,9 @@ from sklearn.linear_model import LogisticRegression
 
 # 1. Independent variables, also called inputs or predictors, don’t depend on other features of interest (or at least for the purpose of the analysis).
 # 2. Dependent variables, also called outputs or responses, depend on the independent variables.
+
+
+# Import the dataset which has flag for social_network_ads purchases across different users with their Gender, Age & Income distribution.
+file = pd.read_csv("/Users/dileepsathyan/Documents/GitHub/datasets/logistic_regression_dataset.csv")
+print(file.head(10))
 

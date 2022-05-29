3ee207814a322c794be6701b3b4404521a7b14d8
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-29 11:27:11 +0400

    Update day_28.py

--- day_28.py
+++ day_28.py
@@ -14,8 +14,8 @@ from sklearn.linear_model import LogisticRegression
 
 
 # Import the dataset which has flag for social_network_ads purchases across different users with their Gender, Age & Income distribution.
-# file = pd.read_csv("/Users/dileepsathyan/Documents/GitHub/datasets/logistic_regression_dataset.csv")
-# print(file.head(10))
+file = pd.read_csv("/Users/dileepsathyan/Documents/GitHub/datasets/logistic_regression_dataset.csv")
+print(file.head(10))
 
 # The nature of the dependent variables differentiates regression and classification problems. Regression problems have continuous and usually unbounded outputs. An example is when you’re estimating the salary as a function of experience and education level. On the other hand, classification problems have discrete and finite outputs called classes or categories. For example, predicting if an employee is going to be promoted or not (true or false) is a classification problem.
 
@@ -24,3 +24,18 @@ from sklearn.linear_model import LogisticRegression
 # 1. Binary or Binomial classification: exactly two classes to choose between (usually 0 and 1, true and false, or positive and negative)
 # 2. Multiclass or multinomial classification: three or more classes of the outputs to choose from.
 
+# Binary classification has four possible types of results:
+
+# True negatives: correctly predicted negatives (zeros)
+# True positives: correctly predicted positives (ones)
+# False negatives: incorrectly predicted negatives (zeros)
+# False positives: incorrectly predicted positives (ones)
+
+
+# Classification Accuracy: 
+# The most straightforward indicator of classification accuracy is the ratio of the number of correct predictions to the total number of predictions (or observations). Other indicators of binary classifiers include the following:
+
+# The 'positive predictive value' is the ratio of the number of true positives to the sum of the numbers of true and false positives.
+# The 'negative predictive value' is the ratio of the number of true negatives to the sum of the numbers of true and false negatives.
+# The 'sensitivity' (aka recall or true positive rate) is the ratio of the number of true positives to the number of actual positives
+# The 'specificity' (or true negative rate) is the ratio of the number of true negatives to the number of actual negatives.

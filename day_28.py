228da2530856ef661c101ca601358937e637dac3
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-29 10:49:53 +0400

    Update day_28.py

--- day_28.py
+++ day_28.py
@@ -1,5 +1,13 @@
-
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
 # LOGISTIC REGRESSION
 
 # Classification is an area of supervised machine learning that tries to predict which class or category some entity belongs to, based on its features
+
+# The features or variables can take one of two forms:
+
+# 1. Independent variables, also called inputs or predictors, don’t depend on other features of interest (or at least for the purpose of the analysis).
+# 2. Dependent variables, also called outputs or responses, depend on the independent variables.
+

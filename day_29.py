f33c4289f7214bc4ce9c4bec93dd42749adb6074
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-30 19:33:10 +0400

    new dataset

---
+++ day_29.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# CAR PRICE PREDICTION USING LINEAR REGRESSION MODELS.
+
+
+# Import the dataset.
+df = pd.read_csv("/Users/dileepsathyan/Documents/GitHub/datasets/car_price_prediction.csv")
+# print(df.head())
+
+
+# Get the gist of the dataset
+# print(df.info())
+
+
+# View all the columns in the dataset
+# print(df.columns)
+

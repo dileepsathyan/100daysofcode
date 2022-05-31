47421b7171942e70487c5c84222ed0114b3eca30
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-05-31 17:20:26 +0400

    Update day_30.py

--- day_30.py
+++ day_30.py
@@ -64,3 +64,15 @@ correct_names('vw', 'volkswagen')
 
 ######################################################################################################
 
+# Data Visualisation using matplotlib library
+
+plt.figure(figsize=(8,4))
+
+plt.subplot(1, 2, 1)
+plt.hist(df['price'], 10)
+plt.title('Car Price Distribution')
+
+plt.subplot(1, 2, 2)
+plt.boxplot(df['price'])
+plt.title('Car Price Spread')
+plt.show()

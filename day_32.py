295159ac5c08d01f0455f393cd5e99ca73863988
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-02 17:34:41 +0400

    Create day_32.py

---
+++ day_32.py
@@ -0,0 +1,14 @@
+# MACHINE LEARNING
+
+# Credits: https://machinelearningmastery.com/a-tour-of-machine-learning-algorithms/
+
+
+### Three different learning styles in ML algorithms.
+
+## 1. Supervised Learning
+# ----- 
+
+## 2. Unsupervised Learning
+
+
+## Semi-supervised Learning

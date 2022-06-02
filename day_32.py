402cf61b86fbef6be80358f2aa27cf4257a0c79f
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-02 17:39:41 +0400

    Update day_32.py

--- day_32.py
+++ day_32.py
@@ -3,12 +3,18 @@
 # Credits: https://machinelearningmastery.com/a-tour-of-machine-learning-algorithms/
 
 
-### Three different learning styles in ML algorithms.
+#### Three different learning styles in ML algorithms.
 
-## 1. Supervised Learning
-# ----- 
+### 1. Supervised Learning:
 
-## 2. Unsupervised Learning
+# ----- This involves training data and a known result for the variables to initially train the model.
+# ----- A model is prepared through a training process which is required to do predictions. It will be corrected when it does wrong outputs. The training process continues until the model achieves a desired level of accuracy using the training data
+# ----- Examples: Classification & Regression models.
 
 
-## Semi-supervised Learning
+### 2. Unsupervised Learning:
+
+
+
+
+### Semi-supervised Learning

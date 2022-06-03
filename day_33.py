1657aab9019a75cb38d0f24f37e34556fe3ebb6e
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-03 17:42:23 +0400

    Create day_33.py

---
+++ day_33.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import explained_variance_score, mean_squared_error, mean_absolute_error, r2_score
+
+
+# KING COUNTY HOUSE PRICE PREDICTION

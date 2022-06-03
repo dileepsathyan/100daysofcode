205c516476f46d1209d84ea0cafdf9bb8a3fe791
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-03 20:44:16 +0400

    Update day_33.py

--- day_33.py
+++ day_33.py
@@ -13,7 +13,7 @@ from sklearn.metrics import explained_variance_score, mean_squared_error, mean_a
 
 df = pd.read_csv("/Users/dileepsathyan/Documents/GitHub/datasets/kc_house_data.csv")
 # print(df.head())
-# print(df.describe())
+print(df.describe())
 # print(df.info())
 
 
@@ -28,24 +28,24 @@ df = pd.read_csv("/Users/dileepsathyan/Documents/GitHub/datasets/kc_house_data.c
 
 
 # Add a new calculated column: 'age' of the house.
-df.insert(5, 'age', (df.year - df.yr_built))
+# df.insert(5, 'age', (df.year - df.yr_built))
 
 # print(df[['year', 'yr_built', 'age']])
 
 
 # Check whether the house has been renovated or not.
-print(df.yr_renovated.head(10))
+# print(df.yr_renovated.head(10))
 
 
 # Since the above field is filled with 0 for those houses which were NOT renovated, lets create a new flag column for renovation.
-df['renovated_flag'] = np.where(df.yr_renovated ==0, 0, 1)
+# df['renovated_flag'] = np.where(df.yr_renovated ==0, 0, 1)
 # print(df.renovated_flag.head(10))
 
 
 # Check the correlation among the variables.
-corr_fields = ['price', 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 
-            'condition', 'grade', 'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated', 'zipcode', 
-            'lat', 'long', 'sqft_living15', 'sqft_lot15']
+# corr_fields = ['price', 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 
+#             'condition', 'grade', 'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated', 'zipcode', 
+#             'lat', 'long', 'sqft_living15', 'sqft_lot15']
 # print(df[corr_fields].corr())
 
 

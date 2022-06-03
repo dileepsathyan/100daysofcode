489fa5d8810a8044142754ff7340515fd67d18ff
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-03 17:50:37 +0400

    Update day_33.py

--- day_33.py
+++ day_33.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from datetime import datetime as dt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import explained_variance_score, mean_squared_error, mean_absolute_error, r2_score

4f31fd0701759ba5508bad5c2ede8e4157a9c6ce
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-05 15:42:24 +0400

    Update day_35.py

--- day_35.py
+++ day_35.py
@@ -12,13 +12,12 @@ from sklearn.linear_model import LinearRegression
 
 # Import the dataset
 df = pd.read_csv("/Users/dileepsathyan/Documents/GitHub/datasets/medical_insurance.csv")
+
 # print(df.head())
-
-
 # print(df.shape)
 # print(df.info())
-
 # print(df.describe())
+
 
 
 # Plot the Gender and Smoker distribution among the dataset.
@@ -28,8 +27,9 @@ df = pd.read_csv("/Users/dileepsathyan/Documents/GitHub/datasets/medical_insuran
 # sns.countplot(df.sex)
 # plt.title('Gender Distribution')
 
-
 # plt.subplot(1, 2, 2)
 # sns.countplot(df.smoker)
 # plt.title('Smoker / Non Smoker Distribution')
 # plt.show()
+
+

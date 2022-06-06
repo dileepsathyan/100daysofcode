4c73cde816adb5b0053a9d1d414de47022e60641
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-06 18:07:46 +0400

    Update day_36.py

--- day_36.py
+++ day_36.py
@@ -72,10 +72,10 @@ mean_charge_for_age = pd.DataFrame(df.groupby(['age'])['charges'].mean().reset_i
 #####################################################################################################################################
 
 # Region Distribution
-plt.figure(figsize=(6,5))
-sns.countplot(data=df.region)
-plt.title('Region Distribution')
-plt.show()
+# plt.figure(figsize=(6,5))
+# sns.countplot(df.region)
+# plt.title('Region Distribution')
+# plt.show()
 
 
 region_and_charges = pd.DataFrame(df.groupby(['sex', 'region'])['charges'].agg([np.mean, np.std])).reset_index()
@@ -89,13 +89,13 @@ region_and_charges = pd.DataFrame(df.groupby(['sex', 'region'])['charges'].agg([
 # plt.title('Children Distribution')
 # plt.show()
 
-# children_and_charges = pd.DataFrame(df.groupby(['sex', 'children'])['charges'].agg([np.mean, np.std])).reset_index()
+children_and_charges = pd.DataFrame(df.groupby(['sex', 'children'])['charges'].agg([np.mean, np.std])).reset_index()
 # print(children_and_charges)
 
 
 
 # Find correlation for price among variables.
-# sns.heatmap(df.corr(), annot=True, cmap='BrBG', cbar=True)
+sns.heatmap(df.corr(), annot=True, cmap='BrBG', cbar=True)
 # plt.show()
 
 
@@ -104,31 +104,31 @@ region_and_charges = pd.DataFrame(df.groupby(['sex', 'region'])['charges'].agg([
 
 # print(df.head())
 
-# df.replace({'sex': {'female': 0, 'male': 1}}, inplace=True)
-# df.replace({'smoker': {'no': 0, 'yes': 1}}, inplace=True)
-# df.replace({'region': {'southeast': 0, 'southwest': 1,
-#                         'northeast': 2, 'northwest': 3}}, inplace=True)
+df.replace({'sex': {'female': 0, 'male': 1}}, inplace=True)
+df.replace({'smoker': {'no': 0, 'yes': 1}}, inplace=True)
+df.replace({'region': {'southeast': 0, 'southwest': 1,
+                        'northeast': 2, 'northwest': 3}}, inplace=True)
 # print(df.head())
 
-# y = df['charges']
-# X = df.drop(columns='charges', axis=1)
+y = df['charges']
+X = df.drop(columns='charges', axis=1)
 
 # print(y.head())
 # print(X.head())
 
 
 # Separate the train and test datasets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=1)
 
 
 # Create a model and fit it
 
-# model = LinearRegression()
-# model.fit(X_train, y_train)
+model = LinearRegression()
+model.fit(X_train, y_train)
 
 
 # Predict the results using the model
-# y_pred = model.predict(X_test)
+y_pred = model.predict(X_test)
 
 
 # Find the MSE and r2 value
@@ -140,8 +140,8 @@ region_and_charges = pd.DataFrame(df.groupby(['sex', 'region'])['charges'].agg([
 
 
 
-# r2_train = r2_score(y_train, model.predict(X_train))
-# r2_test = r2_score(y_test, y_pred)
+r2_train = r2_score(y_train, model.predict(X_train))
+r2_test = r2_score(y_test, y_pred)
 
-# print(f'R Squared Value within train data: {r2_train}')
-# print(f'R Squared Value on test data: {r2_test}')
+print(f'R Squared Value within train data: {r2_train}')
+print(f'R Squared Value on test data: {r2_test}')

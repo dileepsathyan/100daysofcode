1a6b41ec83d3103aa4c44c70ec2c33e25176f27d
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-07 18:08:02 +0400

    Update day_38.py

--- day_38.py
+++ day_38.py
@@ -4,20 +4,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.datasets import make_blobs
-from sklearn.cluster import k_means
+from sklearn.cluster import KMeans
 
 
 # Create a dataset using make_blobs()
 
-data_points = make_blobs(100, n_features=3, centers=5)
-cols = ['feature1', 'feature2', 'feature3']
+data_points = make_blobs(100, n_features=1, centers=3)
 
-df = pd.DataFrame(data_points[0], columns=cols)
-# print(df.head())
+df = pd.DataFrame(data_points[0], columns=['feat_1'])
+print(df.head())
 
 
-# Visualise the dataframe in 3-D
-fig = plt.figure(dpi=100, figsize=(5,5))
-ax = fig.gca(projection='3d')
-ax.scatter(df.feature1, df.feature2, df.feature3) 
-plt.show()
+# Create a model and fit it
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(df)
+

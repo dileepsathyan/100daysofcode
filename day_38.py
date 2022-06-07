70e8678e551fd9d658f9faa6da026c491566d86a
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-07 17:50:04 +0400

    Update day_38.py

--- day_38.py
+++ day_38.py
@@ -3,5 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.datasets import make_blobs
 from sklearn.cluster import k_means
 
+
+# Create a dataset using make_blobs()
+
+data_points = make_blobs(100, n_features=4, centers=5)
+cols = ['feature1', 'feature2', 'feature3', 'feature4']
+
+df = pd.DataFrame(data_points[0], columns=cols)
+print(df.head())

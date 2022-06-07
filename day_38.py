0576f998d8a908342d35bccf3160ce1de0a1c8ca
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-07 18:14:54 +0400

    Update day_38.py

--- day_38.py
+++ day_38.py
@@ -9,13 +9,18 @@ from sklearn.cluster import KMeans
 
 # Create a dataset using make_blobs()
 
-data_points = make_blobs(100, n_features=1, centers=3)
+data_points = make_blobs(100, n_features=2, centers=3)
 
-df = pd.DataFrame(data_points[0], columns=['feat_1'])
-print(df.head())
+df = pd.DataFrame(data_points[0], columns=['feat_1', 'feat_2'])
+# print(df.head())
 
 
 # Create a model and fit it
 kmeans = KMeans(n_clusters=3)
 kmeans.fit(df)
 
+print(kmeans.cluster_centers_)
+
+df['clusters'] = kmeans.labels_
+
+print(df.head(10))

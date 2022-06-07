a1aa82c758634301d87a54c9af1e54e481c56933
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-07 17:54:23 +0400

    Update day_38.py

--- day_38.py
+++ day_38.py
@@ -9,8 +9,15 @@ from sklearn.cluster import k_means
 
 # Create a dataset using make_blobs()
 
-data_points = make_blobs(100, n_features=4, centers=5)
-cols = ['feature1', 'feature2', 'feature3', 'feature4']
+data_points = make_blobs(100, n_features=3, centers=5)
+cols = ['feature1', 'feature2', 'feature3']
 
 df = pd.DataFrame(data_points[0], columns=cols)
-print(df.head())
+# print(df.head())
+
+
+# Visualise the dataframe in 3-D
+fig = plt.figure(dpi=100, figsize=(5,5))
+ax = fig.gca(projection='3d')
+ax.scatter(df.feature1, df.feature2, df.feature3) 
+plt.show()

0cb5a7a2a4017f0c3d3fdec851d30bc8ece452a4
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-07 16:19:14 +0400

    Create day_37.py

---
+++ day_37.py
@@ -0,0 +1,12 @@
+persons = ['Dileep', 'Sachu', 'Trinay', 'Sathyan', 'Latha']
+age = [34, 32, 4, 65, 61]
+
+combined = zip(persons, age)
+# print(list(combined))
+
+
+result = [('Dileep', 34), ('Sachu', 32), ('Trinay', 4), ('Sathyan', 65), ('Latha', 61)]
+
+unzipped = list(zip(*result))
+# print(unzipped)
+

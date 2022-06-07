a0e60ff39de997bb43215f1abede9f176cf999de
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-07 16:43:08 +0400

    Update day_37.py

--- day_37.py
+++ day_37.py
@@ -1,21 +1,27 @@
-persons = ['Dileep', 'Sachu', 'Trinay', 'Sathyan', 'Latha']
+# Zip,  Unzip and Enumerate methods in Python.
+
+name = ['John', 'Smith', 'Adam', 'Filipe', 'Steve']
 age = [34, 32, 4, 65, 61]
 
-combined = zip(persons, age)
-# print(list(combined))
 
 
-result = [('Dileep', 34), ('Sachu', 32), ('Trinay', 4), ('Sathyan', 65), ('Latha', 61)]
+# Zip both the name and age lists in 1 list.
 
-unzipped = list(zip(*result))
-# print(unzipped)
+zipped_list = list(zip(name, age))
+# print(zipped_list)
 
-for i in unzipped:
+
+# Unziping the zipped_list
+zip_list = [('John', 34), ('Smith', 32), ('Adam', 4), ('Filipe', 65), ('Steve', 61)]
+
+unzipped_list = list(zip(*zip_list))
+# print(unzipped_list)
+
+
+# Extract the values from the unzipped_list as separate lists
+for i in unzipped_list:
     print(list(i))
 
 
-ind = zip(range(5), persons)
-# print(list(ind))
-
-for i, item in list(ind):
-    print(i)
+# If we have to index the values in a list.
+countries = ['India', 'USA', 'Canada', 'UAE', 'Australia']

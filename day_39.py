dd78465e6a222df7e37af38a30174949d0c73f32
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-09 17:14:44 +0400

    Create day_39.py

---
+++ day_39.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+
+
+# MALL CUSTOMER DATA CLUSTERING USING KMEANS ALGORITHM

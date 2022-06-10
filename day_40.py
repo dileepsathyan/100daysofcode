9aa4f310bfe2b962b63f1ef41e7aac1f277e2a13
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-10 18:50:42 +0400

    Update day_40.py

--- day_40.py
+++ day_40.py
@@ -18,7 +18,7 @@ df = pd.read_csv('/Users/dileepsathyan/Documents/GitHub/datasets/online_retail.c
 # Understand the datapoints and dataset.
 # print(df.head(10))
 # print(df.describe())
-# print(df.info())
+print(df.info())
 
 
 # Drop the NULL values from the dataset.

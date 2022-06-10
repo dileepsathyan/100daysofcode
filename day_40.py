495767b61d3de9f8d388cce85dff15674a563b05
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-10 18:46:49 +0400

    Update day_40.py

--- day_40.py
+++ day_40.py
@@ -49,20 +49,21 @@ df = df.merge(df1, on='CustomerID', how='left')
 
 # Calculate the recency of each Customer.
 # Find the latest invoice date in the dataframe.
-max_date = max(df['InvoiceDate'])
+# max_date = max(df['InvoiceDate'])
 
 
 # Create a new fiels with the difference between the first InvoiceDate for each user and the latest date in the dataframe.
-df['Recency'] = (max_date - df['InvoiceDate']).dt.days
+# df['Recency'] = (max_date - df['InvoiceDate']).dt.days
 
 
 # Group the Customers to find the first InvoiceDate for each ID.
-df2 = df.groupby(['CustomerID'])['Recency'].min().reset_index()
-df2.rename({'Recency':'RecencyDays'}, axis=1, inplace=True)
+# df2 = df.groupby(['CustomerID'])['Recency'].min().reset_index()
+# df2.rename({'Recency':'RecencyDays'}, axis=1, inplace=True)
 # print(df2.head())
 
 
-df = df.merge(df2, on='CustomerID', how='left')
-df.drop(columns=['Recency'], axis=1, inplace=True)
-df.rename({'RecencyDays':'Recency'}, axis=1, inplace=True)
-print(df.head())
+# Merge the dataframes on CustomerID to get the Recency field onto the original df.
+# df = df.merge(df2, on='CustomerID', how='left')
+# df.drop(columns=['Recency'], axis=1, inplace=True)
+# df.rename({'RecencyDays':'Recency'}, axis=1, inplace=True)
+# print(df.head(10))

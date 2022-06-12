25a7bf9ca1c5b75c43c12eb12ef4c6e087c07c57
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-12 16:34:41 +0400

    Update day_42.py

--- day_42.py
+++ day_42.py
@@ -22,20 +22,20 @@ print(df.describe())
 
 
 # Process the valus in the dataset using StandardScaler
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(df)
-df_scaled = pd.DataFrame(scaled_data, columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
-print(df_scaled.head())
+# scaler = StandardScaler()
+# scaled_data = scaler.fit_transform(df)
+# df_scaled = pd.DataFrame(scaled_data, columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+# print(df_scaled.head())
 
 
 # ELBOW Method: Find the optimal K value
-attempts = range(1,10)
-wss = []
-for k in attempts:
-    model_elbow = KMeans(n_clusters= k, init='k-means++')
-    model_elbow.fit(df_scaled)
-    wss_iter = model_elbow.inertia_
-    wss.append(wss_iter)
+# attempts = range(1,10)
+# wss = []
+# for k in attempts:
+#     model_elbow = KMeans(n_clusters= k, init='k-means++')
+#     model_elbow.fit(df_scaled)
+#     wss_iter = model_elbow.inertia_
+#     wss.append(wss_iter)
 
 # df_wss = pd.DataFrame({'cluster': attempts, 'wss': wss})
 # print(df_wss)
@@ -48,13 +48,13 @@ for k in attempts:
 
 
 # Build the final model with 3 clusters
-model = KMeans(n_clusters=3, init='k-means++')
-model.fit(df_scaled)
-df['cluster'] = model.labels_
+# model = KMeans(n_clusters=3, init='k-means++')
+# model.fit(df_scaled)
+# df['cluster'] = model.labels_
 
-print(df.head())
+# print(df.head())
 
 
 # Plot the results to visulaize the clusters.
-sns.pairplot(df, hue='cluster')
-plt.show()
+# sns.pairplot(df, hue='cluster')
+# plt.show()

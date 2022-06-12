1179ffb608f1de28584934d6cacc8db83c425fe0
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-12 16:40:14 +0400

    Iris Data Clustering (KMeans)

--- day_42.py
+++ day_42.py
@@ -1,4 +1,3 @@
-from lib2to3.pytree import HUGE
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -22,39 +21,39 @@ print(df.describe())
 
 
 # Process the valus in the dataset using StandardScaler
-# scaler = StandardScaler()
-# scaled_data = scaler.fit_transform(df)
-# df_scaled = pd.DataFrame(scaled_data, columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
-# print(df_scaled.head())
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(df)
+df_scaled = pd.DataFrame(scaled_data, columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+print(df_scaled.head())
 
 
 # ELBOW Method: Find the optimal K value
-# attempts = range(1,10)
-# wss = []
-# for k in attempts:
-#     model_elbow = KMeans(n_clusters= k, init='k-means++')
-#     model_elbow.fit(df_scaled)
-#     wss_iter = model_elbow.inertia_
-#     wss.append(wss_iter)
+attempts = range(1,10)
+wss = []
+for k in attempts:
+    model_elbow = KMeans(n_clusters= k, init='k-means++')
+    model_elbow.fit(df_scaled)
+    wss_iter = model_elbow.inertia_
+    wss.append(wss_iter)
 
-# df_wss = pd.DataFrame({'cluster': attempts, 'wss': wss})
-# print(df_wss)
+df_wss = pd.DataFrame({'cluster': attempts, 'wss': wss})
+print(df_wss)
 
 
-# sns.scatterplot(x=df_wss['cluster'], y=df_wss['wss'])
-# plt.show()
+sns.scatterplot(x=df_wss['cluster'], y=df_wss['wss'])
+plt.show()
 
 # Looks like 3 clusters will be good for this dataset but we will reconfirm the same using Silhouette method.
 
 
-# Build the final model with 3 clusters
-# model = KMeans(n_clusters=3, init='k-means++')
-# model.fit(df_scaled)
-# df['cluster'] = model.labels_
 
-# print(df.head())
+# Build the final model with 3 clusters
+model = KMeans(n_clusters=3, init='k-means++')
+model.fit(df_scaled)
+df['cluster'] = model.labels_
+print(df.head())
 
 
 # Plot the results to visulaize the clusters.
-# sns.pairplot(df, hue='cluster')
-# plt.show()
+sns.pairplot(df, hue='cluster')
+plt.show()

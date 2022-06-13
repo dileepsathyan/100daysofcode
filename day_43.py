fc8808d1786095b0d94ebc6e580ab397e210eba7
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-13 16:40:46 +0400

    Update day_43.py

--- day_43.py
+++ day_43.py
@@ -7,3 +7,15 @@ from sklearn import metrics
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
+
+# WINE DATA CLUSTERING
+
+# Import the dataset.
+df = pd.read_csv('/Users/dileepsathyan/Documents/GitHub/datasets/wine_clustering.csv')
+
+# Analyse the dataset
+# print(df.head())
+# print(df.info())
+# print(df.describe())
+
+

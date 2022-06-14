40663a393fe00c21c93c59226c7d6ff7edf73f43
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-14 16:52:19 +0400

    Bank Customer Clustering (KModes): Part 1

--- day_44.py
+++ day_44.py
@@ -10,29 +10,29 @@ from kmodes.kmodes import KModes
 
 # Import the dataset.
 df = pd.read_csv('/Users/dileepsathyan/Documents/GitHub/datasets/bank_marketing.csv')
-# print(df.head())
+print(df.head())
 
 
 # Subset the dataframe only for categorical values.
 df = df[['age', 'job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'day_of_week', 'poutcome']]
-# print(df.head())
+print(df.head())
 
 
 # Convert the age to a categorical variable by binning it into buckets.
 df['age_bins'] = pd.cut(df['age'], 
                         [0, 20, 30, 40, 50, 60, 70, 80, 90, 100], 
                         labels=['0-20', '20-30', '30-40', '40-50', '50-60', '60-70', '70-80', '80-90', '90-100'])
-# print(df.head())
+print(df.head())
 
 
 # Drop the original age column.
 df.drop(columns=['age'], axis=1, inplace=True)
-# print(df.head())
+print(df.head())
 
 
 # Find the stats of the resultant dataframe.
-# print(df.describe())
-# print(df.info())
+print(df.describe())
+print(df.info())
 
 
 # Prepare the data using Label Encoder.

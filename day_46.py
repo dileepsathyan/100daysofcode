f4981a1ed0c6e5ef809c23e7c9811112b20b49db
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-16 17:51:51 +0400

    Update day_46.py

--- day_46.py
+++ day_46.py
@@ -1,9 +1,9 @@
+import matplotlib
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-import sklearn
+sns.set()
 
 
 # CREADIT CARD FRAUD DETECTION
@@ -16,14 +16,17 @@ df = pd.read_csv('/Users/dileepsathyan/Documents/GitHub/datasets/credit_card.csv
 # print(df.describe())
 
 cols = df.columns.to_list()
+cols = cols[1:-1]
+# print(cols)
 
-fig = plt.figure(figsize = (12, 10))
+fig = plt.figure(figsize = (14, 9))
+i = 1
 for col in cols:
-    i = 0
-    plt.subplot(5, 6, i+1)
+    plt.subplot(5, 6, i)
     plt.plot(df[col])
     while i == 29:
         break
     else:
         i += 1
+plt.tight_layout()
 plt.show()

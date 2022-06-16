c5f4b3e8c5aea210eb79099fe9601de476a5b90a
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-16 17:31:01 +0400

    Update day_46.py

--- day_46.py
+++ day_46.py
@@ -4,3 +4,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 import sklearn
+
+
+# CREADIT CARD FRAUD DETECTION
+
+
+# Import the dataset.
+df = pd.read_csv('/Users/dileepsathyan/Documents/GitHub/datasets/credit_card.csv')
+print(df.head())
+print(df.info())
+print(df.describe())
+

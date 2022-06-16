e8aaebfb3182fd66a8c2fccb2969b42089934cfa
Dileep Sathyan <64250821+dileepsathyan@users.noreply.github.com>
2022-06-16 17:53:27 +0400

    Update day_46.py

--- day_46.py
+++ day_46.py
@@ -23,7 +23,7 @@ fig = plt.figure(figsize = (14, 9))
 i = 1
 for col in cols:
     plt.subplot(5, 6, i)
-#     plt.plot(df[col])
+    plt.plot(df[col])
 #     while i == 29:
 #         break
 #     else:
